b5bd59446a5d96a8920e2bb8b7950492d6b3fc0f
Francisco <franciscopaivaknebel@gmail.com>
2019-08-29 18:58:41 -0300

    Parsing project files and translation files.

--- src/files.js
+++ src/files.js
@@ -1,10 +1,8 @@
 import glob from 'fast-glob';
-import path from 'path';
+import { readFileSync } from 'fs';
 
 function readGlob(inputPath, types = ['ts', 'html']) {
   const inputGlob = `${inputPath}/**/*.(${types.join('|')})`
-  console.log('glob', inputGlob);
-  
   return glob(inputGlob);
 }
 
@@ -16,4 +14,43 @@ function getProjectFiles(projectPath) {
   return readGlob(projectPath, ['ts', 'html']);
 }
 
-export { getTranslationFiles, getProjectFiles };
+function parseFile(fileName) {
+  const file = readFileSync(fileName, { encoding: 'utf8' });
+
+  // May start with '{{' and end with '}}', but not necessary.
+  // Internal string is separated by \' or \".
+  // String is filled with multiple dot separated words.
+  // After the string, whitespace with a pipe | separator.
+  // After the operator, the translate pipe.
+  // Ending the regex, whitespace and the optional '}}'.
+  const matched = file.match(
+    /({{)?( )*(\'|\")((\w|\-)\.?)*(\'|\")( )*\|( )*translate( )*(}})?/g
+  );
+
+  // Only return internal string of matches
+  let foundStrings = [];
+  if (matched && matched.length > 0) {
+    foundStrings = matched.map(str => str.match(/(\'|\")(.*)?(\'|\")/)[2]);
+  }
+  return foundStrings;
+}
+
+function parseTranslation(fileName) {
+  const file = readFileSync(fileName, { encoding: 'utf8' });
+
+  return flattenObject(JSON.parse(file));
+}
+
+function flattenObject(obj, parent, res = {}) {
+  for (let key in obj) {
+    let propName = parent ? parent + '.' + key : key;
+    if (typeof obj[key] == 'object') {
+      flattenObject(obj[key], propName, res);
+    } else {
+      res[propName] = obj[key];
+    }
+  }
+  return res;
+}
+
+export { getTranslationFiles, getProjectFiles, parseFile, parseTranslation };
