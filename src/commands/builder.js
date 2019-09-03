f2a059f2a40bced8c8e9391c9e136533eadb2258
Francisco <franciscopaivaknebel@gmail.com>
2019-09-03 16:48:37 -0300

    DIfference between language and input files.

--- src/commands/builder.js
+++ src/commands/builder.js
@@ -1,9 +1,11 @@
 import files from './files';
 import diff from './diffLang';
+import diffInput from './diffInputLang';
 
 export function commandBuilder(program) {
   files(program);
   diff(program);
+  diffInput(program);
 }
 
 export default commandBuilder;
